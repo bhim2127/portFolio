8bb0854835a4814f1cde5da18519feb51bdde931
bhim2127 <bhimaramsvthar@gmail.com>
2025-04-06 01:48:14 +0530

    Update contactRoutes.js

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -4,7 +4,10 @@ import { createContact, getContacts } from '../controllers/contactController.js'
 const router = express.Router();
 
 // Routes for contact
-router.post('/contact', createContact); // POST create a new contact message
+router.post('/contact', (req, res, next) => {
+  console.log('Received POST request at /api/contact:', req.body); // Debug log
+  next();
+}, createContact);
 console.log('POST /contact route registered'); // Debug
 router.get('/contact', getContacts);    // GET all contact messages (optional)
 
