0383e05c3da43b4227827b7adf6dfc0286ec20f3
bhim2127 <bhimaramsvthar@gmail.com>
2025-04-05 22:40:06 +0530

    Update contactRoutes.js

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -4,7 +4,7 @@ import { createContact, getContacts } from '../controllers/contactController.js'
 const router = express.Router();
 
 // Routes for contact
-router.post('/', createContact); // POST create a new contact message
-router.get('/', getContacts); // GET all contact messages (optional)
+router.post('/contact', createContact); // POST create a new contact message
+router.get('/contact', getContacts);    // GET all contact messages (optional)
 
 export default router;
